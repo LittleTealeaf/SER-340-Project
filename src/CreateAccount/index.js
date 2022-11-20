8bdf44c92a005e674ba97b6bcb793019f098f1ec
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-11-20 11:39:16 -0500

    Formatted code

--- src/CreateAccount/index.js
+++ src/CreateAccount/index.js
@@ -1,34 +1,32 @@
 function onValidateCreateAccount(event) {
-	event.preventDefault()
+	event.preventDefault();
 	const form = document.getElementById("createAccountForm");
 
-// name, username, password, password-verify (form-)
+	// name, username, password, password-verify (form-)
 
 	const inputName = form.querySelector("#form-name input");
 	const inputUsername = form.querySelector("#form-username input");
 	const inputPassword = form.querySelector("#form-password input");
-	const inputPasswordVerify = form.querySelector("#form-password-verify input")
-
+	const inputPasswordVerify = form.querySelector("#form-password-verify input");
 
 	var wasValid = true;
-	
+
 	[inputName, inputUsername, inputPassword].forEach((input) => {
 		input.setCustomValidity("");
-		if(input.checkValidity() === false) {
+		if (input.checkValidity() === false) {
 			wasValid = false;
 		}
-	})
+	});
 
-	if(inputPassword.value !== inputPasswordVerify.value) {
+	if (inputPassword.value !== inputPasswordVerify.value) {
 		inputPasswordVerify.setCustomValidity("Passwords do not match");
-		wasValid = false
+		wasValid = false;
 	}
 
-	if(!wasValid) {
-		form.classList.add("was-validated")	
+	if (!wasValid) {
+		form.classList.add("was-validated");
 		return false;
 	}
 
 	return true;
-
 }
