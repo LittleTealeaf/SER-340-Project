ace59f7dee9cc19cb2dd0b0959206048c650ce6c
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-07 23:11:02 -0500

    Some changes

---
+++ src/AdminHome/script.js
@@ -0,0 +1,2 @@
+document.getElementById("button-add-instructor").addEventListener('click', () => window.location.href = "../CreateAccount/index.html?type=Admin")
+document.getElementById("button-logout").addEventListener('click',() => window.location.href = '../Login')
