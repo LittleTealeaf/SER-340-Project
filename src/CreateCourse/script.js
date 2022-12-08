d2fae40593550545a9f7f3574e8a63ad872c618f
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-12-07 22:52:28 -0500

    Added functionality to create course page

--- src/CreateCourse/script.js
+++ src/CreateCourse/script.js
@@ -1,2 +1,3 @@
 document.getElementById("button-back").addEventListener("click", () => window.location.href = "../InstructorHome/")
 document.getElementById("button-logout").addEventListener("click", () => window.location.href = "../Login/");
+//document.getElementById("form-submit").addEventListener('click',() => window.location.href = "../InstructorHome/")
